52147b013127a5e3649c244246edadc8d13cbc1b
Ahmed Dahan <ahmed.dahan.ali@gmail.com>
2026-06-30 22:01:57 +0300

    feat: add publishable config with length, expiration, and max attempts

---
+++ config/otp.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | OTP Length
+    |--------------------------------------------------------------------------
+    |
+    | The number of digits in the OTP code. Default is 6.
+    |
+    */
+
+    'length' => env('OTP_LENGTH', 6),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Expiration Minutes
+    |--------------------------------------------------------------------------
+    |
+    | The number of minutes until an OTP code expires. Default is 5.
+    |
+    */
+
+    'expiration_minutes' => env('OTP_EXPIRATION_MINUTES', 5),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Max Attempts
+    |--------------------------------------------------------------------------
+    |
+    | The maximum number of verification attempts before the OTP is invalidated.
+    | Default is 5.
+    |
+    */
+
+    'max_attempts' => env('OTP_MAX_ATTEMPTS', 5),
+
+];
